b9186d857f32fa3e8b0db5c46d9855bba42946fb
Tu Nombre <tu_email@ejemplo.com>
2026-08-14 00:33:26 -0600

    Iconos para iphone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="icon" type="image/png" href="{{ asset('pizzetos2.png') }}">
 
     {{-- CONFIGURACIÓN PARA IPHONE / IPAD (iOS) --}}
-    <link rel="apple-touch-icon" href="{{ asset('pizzetos.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('logocelular.png') }}">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="apple-mobile-web-app-title" content="Pizzetos">
